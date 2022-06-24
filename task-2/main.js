793b02b2123328d4b1a9bfc86bb7a5e46372d7f1
KananFarmanli <105041431+KananFarmanli@users.noreply.github.com>
2022-06-24 20:19:13 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -12,13 +12,14 @@ prompt.get('input', function (err, result) {
     var input_data = input.split(','); // ['5', '2 4 7 12 9']
     var n = parseInt(input_data[0]); // Massivin element sayı. Nümunə - 5
     let arr = input_data[1].split(' '); // Daxil edilmiş massiv. Nümunə - ['2', '4', '7', '12', '9']
+
     let max = 0
-
-
-for(let i = 0; i<arr.length; i++) {
-
-        if (arr[i]>max)
-    max = arr[i]
-}
-console.log(max) // 34
+    
+    
+    for(let i = 0; i<arr.length; i++) {
+    
+            if (arr[i]>max)
+        max = arr[i]
+    }
+    console.log(max); // 34
 });
